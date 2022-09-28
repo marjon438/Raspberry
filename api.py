0ebcb779abaece1f15878c8a12a2d33cd2712acb
Mårten Jonsson <marten.jonsson90@hotmail.com>
2022-09-28 14:58:11 +0200

    new commit

--- api.py
+++ api.py
@@ -13,40 +13,43 @@ api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
-class VideoModel(db.Model):
+class Todos(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
+    done = db.Column(db.Boolean, nullable=False, default=False)
 
-video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("name", type=str, help="Reqparse help: You need to send a name of the video")
-video_put_args.add_argument("id", type=str, help="Reqparse help: You need to send id of the video")
+db.create_all()
+
+put_args = reqparse.RequestParser()
+put_args.add_argument("name", type=str, help="Reqparse help: You need to send a name of the todo")
 
 
 serializing = {
     "id": fields.Integer,
-    "name": fields.String
+    "name": fields.String,
+    "done": fields.Boolean
 }
 
-class TodoSimple(Resource):
+class TodosApi(Resource):
     
     @marshal_with(serializing)
     def get(self):
-        args = video_put_args.parse_args()
+        args = put_args.parse_args()
         if args["id"] == "":
-            return VideoModel.query.all()
+            return Todos.query.all()
         else:
-            new_video = VideoModel.query.filter_by(id=args["id"]).first()
-            return new_video
+            new_todo = Todos.query.filter_by(id=args["id"]).first()
+            return new_todo
             
         
 
     def post(self):
-        args = video_put_args.parse_args()
-        new_video = VideoModel(name=args["name"])
+        args = put_args.parse_args()
+        new_todo = Todos(name=args["name"])
         if args["name"] == "":
             return {"message":"You need to send a name of the video"}
         else:
-            db.session.add(new_video)
+            db.session.add(new_todo)
             db.session.commit()
             return {"message":"Video added"}
             
@@ -54,12 +57,12 @@ class TodoSimple(Resource):
         
 
     def delete(self):
-        video_delete_args = reqparse.RequestParser()
-        video_delete_args.add_argument("id", type=str, help="Reqparse help: You need to send id of the video", required = True)
-        args = video_delete_args.parse_args()
-        new_video = VideoModel.query.filter_by(id=args["id"]).first()
-        if new_video:
-            db.session.delete(new_video)
+        args = delete_args.reqparse.RequestParser()
+        delete_args.add_argument("id", type=str, help="Reqparse help: You need to send id of the video", required = True)
+        args = delete_args.parse_args()
+        new_todo = Todos.query.filter_by(id=args["id"]).first()
+        if new_todo:
+            db.session.delete(new_todo)
             db.session.commit()
             return {"message":"Video deleted"}
         else:
@@ -68,13 +71,13 @@ class TodoSimple(Resource):
         
     
     def put(self):
-        args = video_put_args.parse_args()
-        new_video = VideoModel.query.filter_by(id=args["id"]).first()
-        if new_video:
+        args = put_args.parse_args()
+        new_todo = Todos.query.filter_by(id=args["id"]).first()
+        if new_todo:
             if args["name"] == "":
                 return {"message":"You need to send a name of the video"}
             else:
-                new_video.name = args["name"]
+                new_todo.name = args["name"]
                 db.session.commit()
             return {"message":"Video updated"}
         else:
---
+++ fetchfromapi.py
@@ -0,0 +1,31 @@
+from urllib import request
+from requests import put, get, delete, post
+
+base = "http://192.168.10.138:5000/helloworld/"
+
+while True:
+    print("1 get")
+    print("2 post")
+    print("3 delete")
+    print("4 put")
+    userinp = input("input: ")
+    if userinp == "1":
+        id = input("Id: ")
+        answer = get(base, data= {"id":id})
+    elif userinp == "2":
+        inp = input("Name: ")
+        answer = post(base, data= {"name":inp})
+    elif userinp == "3":
+        id = input("Id: ")
+        answer = delete(base, data= {"id":id})
+    elif userinp == "4":
+        id = input("Id: ")
+        inp = input("Name: ")
+        answer = put(base, data= {"id":id,"name":inp})
+    else:
+        break
+    print(answer.json())
+
+
+
+
